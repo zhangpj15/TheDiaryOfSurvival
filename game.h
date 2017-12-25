3ea86c693f3f37affb6a74945d6b378d0e4e6057
Chenning Li <liechenning@gmail.com>
2017-12-26 03:23:49 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,81 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QWidget>
+#include <QTimer>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QPropertyAnimation>
+
+#include "player.h"
+#include "emanager.h"
+#include "pmanager.h"
+#include "gmanager.h"
+#include "bmanager.h"
+#include "death.h"
+#include "end.h"
+
+namespace Ui {
+class game;
+}
+
+class game : public QWidget
+{
+    Q_OBJECT
+
+public:
+    QTimer m_timer;
+    int m_time;    // 与 Timer 捆绑的一个时间值
+    int zone=0;
+
+    Emanager m_emanager;
+    Pmanager m_pmanager;
+    gmanager m_gmanager;
+    bmanager m_bmanager;
+
+    death* m_death;// 失败窗口
+    end* m_end;// quit窗口
+    int space=10;
+
+    Player m_player;
+
+    void startGameLoop();   // 开始游戏
+
+    explicit game(QWidget *parent = 0);
+    ~game();
+
+protected:
+    bool bA,bD;// 标识方向按键
+    void keyPressEvent(QKeyEvent* event);// 键盘按键按压
+    void keyReleaseEvent(QKeyEvent* event);// 标识方向释放
+
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent* event);
+
+    void paintEvent(QPaintEvent* event);
+    void renderBorder(QPainter *painter, int rate);
+    void resizeEvent(QResizeEvent* event);
+
+private slots:
+    void slot_timeLoop();   // 游戏循环
+    void slot_btnShowTab();  // 显示说明框
+    void slot_attack();  // 显示说明框
+
+    void slot_gameOver();   // 游戏结束
+    void slot_quitgame(); // 退出游戏
+    void slot_restart(); // 重启游戏
+
+    void slot_no(); // 退出游戏后返回游戏
+    void slot_yes(); // 退出游戏后返回至主界面
+
+signals:
+    void sig_closeGame();
+    void sig_death();
+    void sig_quitgame();
+    void sig_borngoods();
+
+private:
+    Ui::game *ui;
+};
+
+#endif // GAME_H
