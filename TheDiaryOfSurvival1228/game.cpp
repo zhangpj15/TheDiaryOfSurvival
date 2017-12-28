6392e7992a65aee0475ecf56ca731d0e00cbd1e5
Chenning Li <liechenning@gmail.com>
2017-12-29 01:00:48 +0800

    Add files via upload

--- TheDiaryOfSurvival1228/game.cpp
+++ TheDiaryOfSurvival1228/game.cpp
@@ -61,7 +61,7 @@ void game::startGameLoop()
     bloodrate=3;
     space=1;
     zone=0;
-    //m_user="8asd8";
+    timeBtn=0;
 
     //道具价格
     m_price_skill1=5;
@@ -95,8 +95,6 @@ void game::startGameLoop()
     m_gmanager.setActiveRect(width(),height()-70);
     m_bomanager.setActiveRect(width(),height());
 
-    ui->lbluser->setText(returnUser());
-    qDebug()<<returnUser();
     ui->lblAttackMode->setText(m_pmanager.getAttackMode());
     ui->lblLife->setText(QString::number(m_player.getLife(),10));
     ui->lblVolume->setText(QString::number(m_player.getSize()));
@@ -109,10 +107,7 @@ void game::startGameLoop()
     ui->pbarLife->setVisible(true);    //显示能量槽
     ui->pbarLife->setRange(0,m_player.getLife());    //显示能量槽
     ui->pbarLife->setValue(m_player.getLife());
-    //    ui->pbarLife->setStyleSheet(
-    //                "QProgressBar {border: 2px solid grey;border-radius: 5px;background-color: rgba(0,0,0,0);}"
-    //                "QProgressBar::chunk {background-image: url(:/res/config/ico/coldFireBar.png);}");
-    //提示栏的动画效果
+
     static QPropertyAnimation * animate = new QPropertyAnimation(ui->groupGoods,"pos");
     animate->setStartValue(QPoint(width()-160,140));
     animate->setEndValue(QPoint(width()-20,140));
@@ -120,6 +115,7 @@ void game::startGameLoop()
     animate->setDuration(3000);
     ui->btnShowTab->setText("<");
     animate->start();
+
     setFocus();
 }
 
@@ -128,28 +124,18 @@ void game::startGameLoop()
 void game::slot_timeLoop()
 {
     m_time += m_timer.interval();
-
-    //    qDebug()<<m_time;
     sectime=m_time/1000;
     if(m_time%zonerate==0)//控制缩圈
     {
-        //        qDebug()<<"到点啦";
         zone=m_time/zonerate;
     }
     if(m_time%dayrate==0)//控制背景图片
     {
-        //        qDebug()<<"到点啦";
-        //pix=QPixmap(":/res/img/background/playbg1.jpg");
-        //        ui->lblback->setStyleSheet{
-        //                "QProgressBar::chunk {background-image: url(:/res/config/ico/coldFireBar.png);}"}
-        //      mainwindow.setStyleSheet("#mainwindow{border-image:url(:/res/img/background/playbg1.jpg);}");
-        //ui->lblback->show();
         setAutoFillBackground(true);  //这句一定不能少，否则图片显示不出来。
         QPalette palette;
         palette.setBrush(QPalette::Background, QBrush(QPixmap(QString(":/res/img/background/playbg%1.jpg").arg((m_time/dayrate)%15+1))));
         setPalette(palette);
     }
-    //ui->btnShowTab->setText(">");
 
     m_player.updateStates();// 位置刷新
     m_player.updategoods();// 道具刷新
@@ -160,14 +146,12 @@ void game::slot_timeLoop()
         ui->lblLife->setText(QString::number(m_player.getLife(),10));
         ui->lblVolume->setText(QString::number(m_player.getSize()));
         ui->lblSpeed->setText(QString::number(m_player.getSpeed()));
-        //        ui->lblVolume;
     }
     m_gmanager.updategoods(m_player.getCurrentPosi(),m_player.getSize());
     m_pmanager.updateAttackEffect(m_player.getCurrentPosi(),m_player.getSize(),m_player.getDir());// 攻击模式刷新
 
     bool attacked = (m_emanager.updateEnemys(m_player.getCurrentPosi(),m_player.getSize())||m_bmanager.updatebarriers(m_player.getCurrentPosi(),m_player.getSize())||m_bomanager.updateBoss(m_player.getCurrentPosi(),m_player.getSize()));
     bool hurt= (m_pmanager.m_lifeZone.contains(m_player.getCurrentPosi().x()-m_player.getSize()*0.5,m_player.getCurrentPosi().y()-m_player.getSize()*0.5) && m_pmanager.m_lifeZone.contains(m_player.getCurrentPosi().x()-m_player.getSize()*0.5,m_player.getCurrentPosi().y()+m_player.getSize()*0.5)&&m_pmanager.m_lifeZone.contains(m_player.getCurrentPosi().x()+m_player.getSize()*0.5,m_player.getCurrentPosi().y()-m_player.getSize()*0.5)&&m_pmanager.m_lifeZone.contains(m_player.getCurrentPosi().x()+m_player.getSize()*0.5,m_player.getCurrentPosi().y()+m_player.getSize()*0.5));
-    //    bool hurt=m_pmanager.m_lifeZone.contains(m_player.getCurrentPosi().x());
     bool isGameOver=false;
     static int denum=0;
     if(!hurt||attacked)
@@ -176,7 +160,6 @@ void game::slot_timeLoop()
         {
             if(denum<50){
                 denum++;
-                qDebug()<<"无敌中";
             }
             else{
                 denum=0;
@@ -187,16 +170,12 @@ void game::slot_timeLoop()
             static int countblood=0;
             if(countblood>bloodrate)
             {
-
-                QSound::play(":/res/wav/hurt.wav");//受到伤害的声音
+                //QSound::play(":/res/wav/hurt.wav");//受到伤害的声音
                 countblood=0;
                 m_player.setCurrentLife();
                 ui->lblLife->setText(QString::number(m_player.getLife()));
                 ui->pbarLife->setRange(0,100-1);
                 ui->pbarLife->setValue(m_player.getLife());
-                //        ui->pbarLife->setStyleSheet(
-                //                    "QProgressBar {border: 2px solid grey;border-radius: 5px;background-color: rgba(0,0,0,0);}"
-                //                    "QProgressBar::chunk {background-image: url(:/res/config/ico/coldFireBar.png);}");
                 if(m_player.getLife()==0)isGameOver=true;
             }
             else countblood++;
@@ -340,10 +319,6 @@ void game::keyPressEvent(QKeyEvent *event)
         ui->lblAttackMode->setText(m_pmanager.getAttackMode());
         m_pmanager.setAttacked(true);
         break;
-        //    case Qt::Key_K:
-        //        m_pmanager.changeAttackMode();
-        //        ui->lblAttackMode->setText(m_pmanager.getAttackMode());
-        //        break;
     case Qt::Key_U:
         if(m_pmanager.getSkill_1())
         {
@@ -351,24 +326,27 @@ void game::keyPressEvent(QKeyEvent *event)
             m_pmanager.changeAttackMode(1);
             ui->lblAttackMode->setText(m_pmanager.getAttackMode());
             m_pmanager.setAttacked(true);
+            timeBtn++;
         }
         break;
     case Qt::Key_I:
-        if(m_pmanager.getSkill_2())
+        if(m_pmanager.getSkill_2()&&timeBtn<100)
         {
             m_pmanager.setSkill_2(0);
             m_pmanager.changeAttackMode(2);
             ui->lblAttackMode->setText(m_pmanager.getAttackMode());
             m_pmanager.setAttacked(true);
+            timeBtn++;
         }
         break;
     case Qt::Key_O:
-        if(m_pmanager.getSkill_3())
+        if(m_pmanager.getSkill_3()&&timeBtn<100)
         {
             m_pmanager.setSkill_3(0);
             m_pmanager.changeAttackMode(3);
             ui->lblAttackMode->setText(m_pmanager.getAttackMode());
             m_pmanager.setAttacked(true);
+            timeBtn++;
         }
         break;
     case Qt::Key_Space:
@@ -378,9 +356,8 @@ void game::keyPressEvent(QKeyEvent *event)
             m_timer.start();
         break;
     case Qt::Key_Q:
-        //        m_timer.stop();
-        //        m_end->show();
-        emit sig_quitgame();
+        if(m_player.getLife())
+            emit sig_quitgame();
         break;
     }
 }
@@ -475,15 +452,19 @@ void game::keyReleaseEvent(QKeyEvent *event)
         break;
     case Qt::Key_J:
         m_pmanager.setAttacked(false);
+        timeBtn=0;
         break;
     case Qt::Key_U:
         m_pmanager.setAttacked(false);
+        timeBtn=0;
         break;
     case Qt::Key_I:
         m_pmanager.setAttacked(false);
+        timeBtn=0;
         break;
     case Qt::Key_O:
         m_pmanager.setAttacked(false);
+        timeBtn=0;
         break;
     }
 }
@@ -524,6 +505,7 @@ void game::slot_gameOver()
 {
     m_timer.stop();
     m_death->show();
+    m_death->setWindowModality(Qt::ApplicationModal);
     m_death->move((this->width() - m_death->width())/2,(this->height() - m_death->height())/2);
 }
 
@@ -548,7 +530,6 @@ void game::slot_no()
 void game::slot_yes()
 {
     m_end->hide();
-    bgsound->stop();
     emit sig_closeGame();
 }
 
@@ -581,12 +562,7 @@ void game::slot_learnSkill3()
         ui->lblskill1->setText(QString::number(m_pmanager.getSkill_3()));
     }
 }
-void game::setUser(QString a){
-    m_user=a;
-}
-QString game::returnUser(){
-    return m_user;
-}
+
 //    绘制火焰
 void game::renderBorder(QPainter *painter, int rate)
 {
